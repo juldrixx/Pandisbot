a14216c7c4e9222d721f60c2a734b9c123287f5e
Julien Guitton <juldrixx@gmail.com>
2020-05-07 23:48:37 +0200

    [FEATURE] Mise en place de la structure du projet | Ajout de la commande \!almanax

---
+++ commands/almanax.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const fs = require('fs');
+const { MessageEmbed } = require('discord.js');
+const picture_url = 'http://staticns.ankama.com/dofus/www/game/items/200/$item.png';
+const date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+module.exports = {
+  name: '!almanax',
+  description: 'Get the current almanax!',
+  execute(msg, args) {
+    fs.readFile('./utils/almanax.json', (err, data) => {
+      if (err) throw err;
+      let alamanax = JSON.parse(data);
+      
+      let today = new Date();
+      let current_date = today.toLocaleDateString('fr-FR', date_options);
+      let dd = String(today.getDate()).padStart(2, '0');
+      let mm = String(today.getMonth() + 1).padStart(2, '0');
+
+      let current_almanax = alamanax[mm + '-' + dd];
+      if (current_almanax) {
+        const embed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(current_date[0].toUpperCase() + current_date.slice(1))
+          .setDescription(current_almanax.offering)
+          .setThumbnail(picture_url.replace('$item', current_almanax.objectID))
+          .addField(current_almanax.bonusTitle, current_almanax.bonusDescription)
+          .setTimestamp()
+          .setFooter('Pandisbot');
+        msg.channel.send(embed);
+      }
+      else {
+        msg.channel.send('Wow!! This day isn\'t our database, that\'s weird!');
+      }
+    });
+  },
+};
---
+++ commands/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  Almanax: require('./almanax'),
+};
